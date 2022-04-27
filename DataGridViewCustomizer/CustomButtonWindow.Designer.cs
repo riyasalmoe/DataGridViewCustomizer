878f9cf7dfc0b41f3cc57d96c4279c963668fcc8
Riyas Aboobaker <riyas.almoe@gmail.com>
2022-04-27 10:50:56 +0400

    price button matrix with flowlayout panel

--- DataGridViewCustomizer/CustomButtonWindow.Designer.cs
+++ DataGridViewCustomizer/CustomButtonWindow.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomButtonWindow));
             this.ProductsPanel = new System.Windows.Forms.Panel();
             this.productButton1 = new AroniumButton.ProductButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ProductsPanel
@@ -37,22 +40,61 @@
             this.ProductsPanel.BackColor = System.Drawing.Color.Khaki;
             this.ProductsPanel.Location = new System.Drawing.Point(0, 0);
             this.ProductsPanel.Name = "ProductsPanel";
-            this.ProductsPanel.Size = new System.Drawing.Size(850, 585);
+            this.ProductsPanel.Size = new System.Drawing.Size(251, 123);
             this.ProductsPanel.TabIndex = 0;
+            this.ProductsPanel.Visible = false;
             // 
             // productButton1
             // 
             this.productButton1.BackColor = System.Drawing.Color.Black;
-            this.productButton1.Location = new System.Drawing.Point(884, 107);
+            this.productButton1.Location = new System.Drawing.Point(0, 368);
             this.productButton1.Name = "productButton1";
             this.productButton1.Size = new System.Drawing.Size(170, 195);
             this.productButton1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(386, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(698, 567);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(0, 144);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 200);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Biriyani";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // CustomButtonWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1096, 591);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.productButton1);
             this.Controls.Add(this.ProductsPanel);
             this.Name = "CustomButtonWindow";
@@ -66,5 +108,7 @@
 
         private System.Windows.Forms.Panel ProductsPanel;
         private AroniumButton.ProductButton productButton1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        public System.Windows.Forms.Button button1;
     }
 }
--- DataGridViewCustomizer/CustomButtonWindow.cs
+++ DataGridViewCustomizer/CustomButtonWindow.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,7 +20,7 @@ namespace DataGridViewCustomizer
         {
             InitializeComponent();
             InitData();
-            InitProducts();
+            //InitProducts();
 
         }
 
@@ -79,7 +80,45 @@ namespace DataGridViewCustomizer
 
         private void CustomButtonWindow_Load(object sender, EventArgs e)
         {
+            //FlowLayoutPanel flp = new FlowLayoutPanel();
+            //List<Button> btn = new List<Button>();
+            int i = 0;
+            foreach (var item in products)
+            {
+                //AroniumButton.ProductButton btn = new AroniumButton.ProductButton(item.ProductName, item.ProductPrice);
+                Button btn = new Button();                            
 
+                btn.Name = $"{button1.Name}{i}";
+                btn.FlatAppearance.BorderColor = button1.FlatAppearance.BorderColor;
+                btn.FlatAppearance.MouseOverBackColor = button1.FlatAppearance.MouseOverBackColor;
+                btn.FlatAppearance.BorderColor = button1.FlatAppearance.BorderColor;
+                btn.FlatAppearance.BorderSize = button1.FlatAppearance.BorderSize;
+                btn.BackColor = button1.BackColor;
+                btn.ForeColor = button1.ForeColor;
+                btn.Image = button1.Image;
+                btn.ImageAlign = button1.ImageAlign;
+                btn.Size = button1.Size;
+                btn.Text = $"{item.ProductName}\n{item.ProductPrice:N2}";
+                btn.Font = button1.Font;
+                btn.TextAlign = button1.TextAlign;
+                btn.Click += Btn_Click;
+                btn.Tag = item;
+                flowLayoutPanel1.Controls.Add(btn);
+                
+
+                i += 1;
+            }
+            //foreach (var button in btn)
+            //{
+            //}
+            this.Controls.Add(flowLayoutPanel1);
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            var a = (Button)sender;
+            var b = (Product)a.Tag;
+            MessageBox.Show($"Button {b.ProductName} / {b.ProductPrice:N2} clicked!");
         }
     }
 }
